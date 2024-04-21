2145da06271493844aed1c7138f9474410830900
Stefan Martin <foxkill@gmx.de>
2024-04-21 13:07:57 +0200

    Implement Step 4 of exam-one

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\TaskController;
+use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware(Authenticate::using('sanctum'));
+
+Route::apiResource('tasks', TaskController::class); 

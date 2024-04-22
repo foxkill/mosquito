195a964d0edecb64bad493027fb751526f86747e
Stefan Martin <foxkill@gmx.de>
2024-04-22 12:31:19 +0200

    Make names short TaskRead -> Read

--- app/Enums/TaskTokenEnum.php
+++ app/Enums/TaskTokenEnum.php
@@ -3,9 +3,9 @@
 namespace App\Enums;
 
 enum TaskTokenEnum: string {
-    case TaskList = 'task-list';
-    case TaskRead = 'task-read';
-    case TaskCreate = 'task-create';
-    case TaskUpdate = 'task-update';
-    case TaskDelete = 'task-delete';
+    case List = 'task-list';
+    case Read = 'task-read';
+    case Create = 'task-create';
+    case Update = 'task-update';
+    case Delete = 'task-delete';
 }

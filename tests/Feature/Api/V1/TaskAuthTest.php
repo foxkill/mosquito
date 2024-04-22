7cb95c52e82962f51f135f33b54ab258ddd7bab2
Stefan Martin <foxkill@gmx.de>
2024-04-22 15:47:37 +0200

    Add tests for correctly accessing the routes

---
+++ tests/Feature/Api/V1/TaskAuthTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature\Api\V1;
+
+use App\Enums\TaskTokenEnum;
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class TaskAuthTest extends TestCase
+{
+    /**
+     * It should make sure that accessing the 
+     * read route with incorrect ablities is denied.
+     */
+    public function test_that_read_route_forbidden(): void
+    {
+        // Arrange.
+        $user = User::factory()->create();
+        $task = Task::factory()->create(
+            ['user_id' => $user->id]
+        );
+
+        // Act - as sanctum user.
+        Sanctum::actingAs($user, [TaskTokenEnum::Delete->value]);
+        $response = $this
+            ->getJson(route('tasks.show', $task));
+
+        // Assert - that access is denied.
+        $response->assertStatus(Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * It should make sure that accessing the 
+     * read route is allowed.
+     */
+    public function test_that_read_route_is_allowed(): void
+    {
+        // Arrange.
+        $user = User::factory()->create();
+        $task = Task::factory()->create(
+            ['user_id' => $user->id]
+        );
+
+        // Act - as sanctum user.
+        Sanctum::actingAs($user, [TaskTokenEnum::Read->value]);
+        $response = $this
+            ->getJson(route('tasks.show', $task));
+
+        // Assert - that access is denied.
+        $response->assertStatus(Response::HTTP_OK);
+    }
+}

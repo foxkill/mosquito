da812b398cc598d4035725d0b92697fde735dc31
Stefan Martin <foxkill@gmx.de>
2024-04-22 09:16:02 +0200

    Add capability task-read to token enum

--- app/Enums/TaskTokenEnum.php
+++ app/Enums/TaskTokenEnum.php
@@ -4,6 +4,7 @@ namespace App\Enums;
 
 enum TaskTokenEnum: string {
     case TaskList = 'task-list';
+    case TaskRead = 'task-read';
     case TaskCreate = 'task-create';
     case TaskUpdate = 'task-update';
     case TaskDelete = 'task-delete';

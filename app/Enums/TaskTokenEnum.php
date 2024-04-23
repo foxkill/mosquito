0e321291544d9c0e04dbe037eeb9e49e023eaa98
Stefan Martin <foxkill@gmx.de>
2024-04-23 12:04:39 +0200

    Extend functionality of TaskTokenEnum enum

--- app/Enums/TaskTokenEnum.php
+++ app/Enums/TaskTokenEnum.php
@@ -2,10 +2,28 @@
 
 namespace App\Enums;
 
+/**
+ * Implement a token. 
+ * 
+ * TODO: Move later to a more appropriate location like: 
+ * \App\Auth\Tokens.
+ */
 enum TaskTokenEnum: string {
+    public const NAME = 'access-token';
+
     case List = 'task-list';
     case Read = 'task-read';
     case Create = 'task-create';
     case Update = 'task-update';
     case Delete = 'task-delete';
+
+    /**
+     * Return a token ablity representation.
+     * 
+     * @return string 
+     */
+    public function toAbility(): string
+    {
+        return 'ability:' . $this->value;
+    }
 }

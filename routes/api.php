0ab9ca497a59033ba1b84fafe606042b1cc2f982
Stefan Martin <foxkill@gmx.de>
2024-04-23 12:11:18 +0200

    Use a less error prone approach to set the abilities for the routes

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,12 @@
 <?php
 
+use App\Enums\TaskTokenEnum;
 use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Middleware\Authenticate;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Http\Request;
-use App\Enums\TaskTokenEnum;
 use App\Http\Controllers\AuthController;
-use App\Models\User;
-use Illuminate\Validation\Rules\Enum;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -17,11 +14,25 @@ Route::get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'v1', 'namespace' => '\App\Http\Controllers\Api\V1'], function () {
     // Route::apiResource('tasks', TaskController::class)->middleware(['auth:sanctum']);
-    Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index')->middleware(['auth:sanctum', 'ability:task-list']);
-    Route::post('tasks', [TaskController::class, 'store'])->name('tasks.store')->middleware(['auth:sanctum', 'ability:task-create']);
-    Route::get('tasks/{task}', [TaskController::class, 'show'])->name('tasks.show')->middleware(['auth:sanctum', 'ability:task-read']);
-    Route::put('tasks/{task}', [TaskController::class, 'update'])->name('tasks.update')->middleware(['auth:sanctum', 'ability:task-update']);
-    Route::delete('tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy')->middleware(['auth:sanctum', 'ability:task-delete']);
+    Route::get('tasks', [TaskController::class, 'index'])
+        ->name('tasks.index')
+        ->middleware(['auth:sanctum', TaskTokenEnum::List->toAbility()]);
+
+    Route::post('tasks', [TaskController::class, 'store'])
+        ->name('tasks.store')
+        ->middleware(['auth:sanctum', TaskTokenEnum::Create->toAbility()]);
+
+    Route::get('tasks/{task}', [TaskController::class, 'show'])
+        ->name('tasks.show')
+        ->middleware(['auth:sanctum', TaskTokenEnum::Read->toAbility()]);
+
+    Route::put('tasks/{task}', [TaskController::class, 'update'])
+        ->name('tasks.update')
+        ->middleware(['auth:sanctum', TaskTokenEnum::Update->toAbility()]);
+
+    Route::delete('tasks/{task}', [TaskController::class, 'destroy'])
+        ->name('tasks.destroy')
+        ->middleware(['auth:sanctum',TaskTokenEnum::Delete->toAbility()]);
 });
 
 Route::post('/login', [AuthController::class, 'login']);

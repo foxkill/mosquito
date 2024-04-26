ef093ca7d6578660a2e25874b53ca46f787c5de0
Stefan Martin <foxkill@gmx.de>
2024-04-26 16:40:13 +0200

    Add tests for deadline and tasks.projects

---
+++ tests/Feature/Api/V1/TaskDeadlineTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\Api\V1;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TaskDeadlineTest extends TestCase
+{
+
+    // We must use this trait to create tables.
+    use RefreshDatabase;
+    // To use faker within this test class.
+    use WithFaker;
+    use WithFaker;
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_deadline(): void
+    {
+    }
+}
---
+++ tests/Feature/Api/V1/TaskProjectTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature\Api\V1;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Project;
+use App\Models\User;
+use App\Models\Task;
+use Tests\TestCase;
+
+class TaskProjectTest extends TestCase
+{
+    // We must use this trait to create tables.
+    use RefreshDatabase;
+    // To use faker within this test class.
+    use WithFaker;
+
+    /**
+     * It should be able to read all tasks for a project.
+     */
+    public function test_read_tasks_for_a_project(): void
+    {
+        // Arrange.
+        $user = User::factory()->create();
+        $otherUser = User::factory()->create();
+
+        $projects =  Project::factory(3)->create();
+
+        $tasks = Task::factory(3)->create([
+            'project_id' => $projects[0]->id,
+            'user_id' => $user->id,
+        ]);
+
+        $tasksWithDiffrentProject = Task::factory(3)->create([
+            'project_id' => $projects[2]->id,
+            'user_id' => $user->id,
+        ]);
+
+        $tasksWithoutProject = Task::factory(2)->create([
+            'user_id' => $user->id,
+        ]);
+            
+        $tasksOtherUser = Task::factory(2)->create([
+            'project_id' => $projects[1]->id,
+            'user_id' => $otherUser->id,
+        ]);
+
+        $response = $this->getJson(route('tasks.projects'));
+
+        // Act.
+        dump($tasks->toArray());
+        dd($tasksOtherUser->toArray());
+        // Assert.
+    }
+}
